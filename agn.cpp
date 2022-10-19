2d469ee651749b86ce5d65f4cbb74458a9602e0e
neophyte0903 <100983473+neophyte0903@users.noreply.github.com>
2022-10-19 20:03:22 +0530

    assignment cpp
    copy of code for assignment submission

---
+++ agn.cpp
@@ -0,0 +1,22 @@
+//Write a program to accept two integers and check whether they are equal or not?
+   /* int a = 1, b = 2;
+    if (a == b) {
+        cout << "The two integers are equal";
+    }
+    else {
+        cout << "The two integers are not equal";
+        */
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a , b;
+cin >> a >> b ;
+
+if (a == b) {
+    cout << "The two integers are equal";
+    return 0;
+    }}
